abb958bce39a0befcf26c4044fa386f65167916e
James Coffman <jamesc4023@gmail.com>
2022-04-06 13:09:16 -0400

    commit tree 3ae36c1ac1bae3362fe1b40944c2afc2a74ef2c7

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+function findMatching(arr, str){
+    const matchingNames = arr.filter(element => element.toLowerCase() === str.toLowerCase());
+    return matchingNames;
+}
+
+//test each letter to see if it is in the element string use str.length
+function fuzzyMatch(arr, str){
+    const fuzzyWuzzy = arr.filter(element => element.substring(0, str.length) === str);
+    return fuzzyWuzzy;
+}
+
+function matchName(arr, str){
+    return arr.filter(element => element.name === str);
+}
